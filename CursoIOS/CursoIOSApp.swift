1cd7d2fa603be12c0bd376ad50f1b9d807131cfd
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-06-11 12:09:49 -0500

    Initial Commit

---
+++ CursoIOS/CursoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoIOSApp.swift
+//  CursoIOS
+//
+//  Created by Brandon Luis Meneses Solorzano on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
